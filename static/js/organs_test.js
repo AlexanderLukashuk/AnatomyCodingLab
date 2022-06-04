ea43c807737504899f96cfa2ec2800fe27a6b96c
Саня <aleksandrlukasuk@MacBook-Air-Aleksandr.local>
2022-06-04 21:20:28 +0600

    Вопросы из базы данных не достаются, приехали, конечная, вопросов не будет, всем 5-ки за тест

--- static/js/organs_test.js
+++ static/js/organs_test.js
@@ -1,10 +1,14 @@
+const {uri} = require("../../Xconfig");
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+// const controller = require("../controllers/adminController")
 
 let shuffledQuestions, currentQuestionIndex
+
+AddQuestion();
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
@@ -76,37 +80,93 @@ function clearStatusClass(element) {
   element.classList.remove('wrong')
 }
 
-const questions = [
-  {
-    question: 'What does not apply to the internal organs of a person?',
-    answers: [
-      { text: 'aorta, the pulmonary venies, an arterioles', correct: true },
-      { text: 'pancreas, intestines, colon, rectum, spleen, kidneys, and appendicitis.', correct: false },
-      { text: 'trachea, esophagus, lungs, heart, stomach, liver, gallbladder', correct: false }
-    ]
-  },
-  {
-    question: 'How many hemispheres does the brain consist of?',
-    answers: [
-      { text: '2', correct: true },
-      { text: '1', correct: false },
-      { text: '4', correct: false }
-    ]
-  },
-  {
-    question: 'How is the brain attached to the skull?',
-    answers: [
-      { text: 'nothing', correct: false },
-      { text: 'nerves and blood vessels', correct: true },
-      { text: 'a special mixture that is produced by the brain', correct: false }
-    ]
-  },
-  {
-    question: 'What is the largest organ in the human body?',
-    answers: [
-      { text: 'lungs', correct: false },
-      { text: 'liver', correct: true },
-      { text: 'brain', correct: false }
-    ]
-  }
-]
+const MongoClient = require("mongodb").MongoClient;
+
+// const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(uri);
+const questions = [];
+
+// Подключаемся к серверу
+mongoClient.connect(function(err, client){
+
+  // обращаемся к базе данных admin
+  const db = client.db("test.users");
+
+  // for (let i = 0; i < dbQuestions.length; i++) {
+    questions.push(db.);
+  // }
+
+  // db.command({ping: 1}, function(err, result){
+  //   if(!err){
+  //     console.log("Подключение с сервером успешно установлено");
+  //     console.log(result);
+  //   }
+  //   // Закрываем подключение
+  //   client.close();
+  //   console.log("Подключение закрыто");
+  // });
+});
+
+// const dbQuestions = [];
+//
+// // function getQuestions() {
+// //   // dbQuestions.push(db.question.find());
+// //   dbQuestions.push(controller.getTests);
+// // }
+//
+// getQuestions()
+
+// console.log(questions[1]);
+
+// const questions = [];
+
+// function AddQuestion() {
+//   for (let i = 0; i < dbQuestions.length; i++) {
+//     questions.push(dbQuestions[i]);
+//   }
+// }
+
+
+
+// const questions = [
+//   {
+//     question: 'What does not apply to the internal organs of a person?',
+//     answers: [
+//       { text: 'aorta, the pulmonary venies, an arterioles', correct: true },
+//       { text: 'pancreas, intestines, colon, rectum, spleen, kidneys, and appendicitis.', correct: false },
+//       { text: 'trachea, esophagus, lungs, heart, stomach, liver, gallbladder', correct: false }
+//     ]
+//   },
+//   {
+//     question: 'How many hemispheres does the brain consist of?',
+//     answers: [
+//       { text: '2', correct: true },
+//       { text: '1', correct: false },
+//       { text: '4', correct: false }
+//     ]
+//   },
+//   {
+//     question: 'How is the brain attached to the skull?',
+//     answers: [
+//       { text: 'nothing', correct: false },
+//       { text: 'nerves and blood vessels', correct: true },
+//       { text: 'a special mixture that is produced by the brain', correct: false }
+//     ]
+//   },
+//   {
+//     question: 'What is the largest organ in the human body?',
+//     answers: [
+//       { text: 'lungs', correct: false },
+//       { text: 'liver', correct: true },
+//       { text: 'brain', correct: false }
+//     ]
+//   },
+//   {
+//     question: question.question.text,
+//     answers: [
+//       { text: question.answer1.text, correct: true },
+//       { text: question.answer2.text, correct: false },
+//       { text: question.answer3.text, correct: false }
+//     ]
+//   }
+// ]

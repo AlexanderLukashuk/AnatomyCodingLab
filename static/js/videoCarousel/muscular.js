a7c269eee5e968faddc64ab059f672fb5c3c91a9
Саня <aleksandrlukasuk@MacBook-Air-Aleksandr.local>
2022-06-04 21:57:06 +0600

    Добавлен скрипт на страницу мышц, чтобы открывались видео

---
+++ static/js/videoCarousel/muscular.js
@@ -0,0 +1,27 @@
+$("#muscles_1").click(function() {
+    window.open("https://www.youtube.com/watch?v=pMw5cvmnE90");
+});
+
+$("#muscles_2").click(function() {
+    window.open("https://www.youtube.com/watch?v=McwTH-aHh5s");
+});
+
+$("#muscles_3").click(function() {
+    window.open("https://www.youtube.com/watch?v=rDaAnv4M2oo");
+});
+
+$("#muscles_4").click(function() {
+    window.open("https://www.youtube.com/watch?v=Ktv-CaOt6UQ");
+});
+
+$("#muscles_5").click(function() {
+    window.open("https://www.youtube.com/watch?v=1h5l78st7wQ");
+});
+
+$("#muscles_6").click(function() {
+    window.open("https://www.youtube.com/watch?v=C747_VeBAvA");
+});
+
+$(".btn-watch").css({
+    "color": "#62D0DF"
+});

9df5dd2388c5f4f2bec1908dd8d092b0ff102850
Саня <aleksandrlukasuk@MacBook-Air-Aleksandr.local>
2022-06-04 22:15:55 +0600

    Добавлена видео-карусель на страницу скелета и скрипт, чтобы открывалось видео

---
+++ static/js/videoCarousel/skeleton.js
@@ -0,0 +1,31 @@
+ $("#skeleton_1").click(function() {
+     window.open("https://www.youtube.com/watch?v=PMsNGjD2iBQ");
+ });
+
+$("#skeleton_2").click(function() {
+    window.open("https://www.youtube.com/watch?v=Bh7pQ3mwz2Q");
+});
+
+$("#skeleton_3").click(function() {
+    window.open("https://www.youtube.com/watch?v=jgyBTW7vTAw");
+});
+
+$("#skeleton_4").click(function() {
+    window.open("https://www.youtube.com/watch?v=qvrUL_TPUmI");
+});
+
+$("#skeleton_5").click(function() {
+    window.open("https://www.youtube.com/watch?v=f-FF7Qigd3U");
+});
+
+$("#skeleton_6").click(function() {
+    window.open("https://www.youtube.com/watch?v=rDGqkMHPDqE");
+});
+
+ $("#skeleton_7").click(function() {
+     window.open("https://www.youtube.com/watch?v=BsnKZDGSuw0");
+ });
+
+$(".btn-watch").css({
+    "color": "#62D0DF"
+});

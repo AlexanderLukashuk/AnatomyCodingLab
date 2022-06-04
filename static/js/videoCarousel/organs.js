1d5a4c5cba2cbc4fec38711b76e1b20533a803cb
Саня <aleksandrlukasuk@MacBook-Air-Aleksandr.local>
2022-06-04 22:32:23 +0600

    Добавлена видео-карусель на страницу внутренних органов и скрипт для просмотра видео

---
+++ static/js/videoCarousel/organs.js
@@ -0,0 +1,31 @@
+ $("#organs_1").click(function() {
+     window.open("https://www.youtube.com/watch?v=kV8zgacFmn4");
+ });
+
+$("#organs_2").click(function() {
+    window.open("https://www.youtube.com/watch?v=jePyncSftX4");
+});
+
+$("#organs_3").click(function() {
+    window.open("https://www.youtube.com/watch?v=jJ6DlTjm8hc");
+});
+
+$("#organs_4").click(function() {
+    window.open("https://www.youtube.com/watch?v=rU7fkv0UTPg");
+});
+
+$("#skeleton_5").click(function() {
+    window.open("https://www.youtube.com/watch?v=f-FF7Qigd3U");
+});
+
+$("#skeleton_6").click(function() {
+    window.open("https://www.youtube.com/watch?v=rDGqkMHPDqE");
+});
+
+$("#skeleton_7").click(function() {
+    window.open("https://www.youtube.com/watch?v=BsnKZDGSuw0");
+});
+
+$(".btn-watch").css({
+    "color": "#62D0DF"
+});
